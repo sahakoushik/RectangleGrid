0754429378fb5a8cc0f0aefd781c5acf35c6735a
Koushik Saha <sahakoushikkumar@gmail.com>
2024-02-10 19:02:36 +0600

    ui update

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import React, { Component } from 'react'
 import Header from './components/Header'
 import Rectangle from './components/Rectangle'
-import MainRectangle from './components/MainRectangle'
-
 export class App extends Component {
   render() {
     return (
       <View style={{flex:1}}>
-        {/* <MainRectangle/> */}
         <Header/>
         <Rectangle/>
       </View>
